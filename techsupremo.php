73c7d8471810f2e56f0600f20d6ac8127a7a9ebf
Your Name <you@example.com>
2021-02-02 14:43:58 +0800

    techsupremo

---
+++ techsupremo.php
@@ -0,0 +1,9 @@
+ <?php
+echo "TechSupremo";
+echo "Members:";
+echo "Alonzo, Arlene Grace C.";
+echo "Dela Cruz, Diane Louise M.";
+echo "Diamzon Jhon Paul M.";
+echo "Ombao, Steve Martin O.";
+echo "Soriano, Justine G.";
+?>
